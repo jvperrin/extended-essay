ca31c3dae6d1e888a7a81b73b215d4b77d80f19d
Jason Perrin <jasonvperrin@gmail.com>
2014-08-10 23:09:36 -0600

    Change device and loop through entire contents

--- test.cpp
+++ test.cpp
@@ -8,9 +8,14 @@ using namespace std;
 
 int main () {
   const int BLOCK_SIZE = 512; // Number of bytes per logical sector, `sudo fdisk -l` shows the sector size (logical / physical).
-  const int NUM_BLOCKS = 374859776; // Number of blocks of size BLOCKSIZE in the device.
-  const int BUFFER_SIZE = 1000000; // Size of the buffer used for reading the data.
-  const char* filename = "/dev/sda6";
+  const int NUM_BYTES = 2146435072; // Number of bytes in the device.
+  const int BUFFER_SIZE = 1048576 * 2; // Size of the buffer used for reading the data.
+  // 1MB: 1048576 timing
+  // real	22m33.236s
+  // user	0m0.100s
+  // sys	0m39.942s
+  // 16MB: 16769024 doesn't run?
+  const char* filename = "/dev/sda7";
 
   char buffer[BUFFER_SIZE];
 
@@ -19,21 +24,19 @@ int main () {
   if (device.is_open()) {
     cout << "Opened!" << endl;
 
-    device.seekg (0, device.end);
-    cout << "Length of device: " << device.tellg() << " bytes" << endl;
-  	device.seekg (0, device.beg);
+    // device.seekg (0, device.end);
+    // cout << "Length of device: " << device.tellg() << " bytes" << endl;
+    // device.seekg (0, device.beg);
 
-    for(int i = 0; i < 100; i++) {
-    // for(int i = 0; i < NUMBLOCKS; i++) {
+    for(int i = 0; i < (NUM_BYTES/BUFFER_SIZE); i++) {
       cout << i << endl;
       device.read(buffer, BUFFER_SIZE);
       cout.write(buffer, BUFFER_SIZE);
-      // usleep(3000);
     }
 
     device.close();
   } else {
-    cout << "Unable to open /dev/sda6." << endl;
+    cout << "Unable to open " << filename << endl;
   }
 
   return 0;
